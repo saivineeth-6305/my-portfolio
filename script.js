aee2380fe37d4d8505ca73f246b7a54254d4d060
Saivineeth <psv12082001@outlook.com>
2025-04-29 21:11:05 +0530

    Update script.js  -- header hiding functionality

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY === 0) {
+    // Show header when at the top of the page
+    header.classList.remove("hidden");
+  } else {
+    // Hide header when not at the top
+    header.classList.add("hidden");
+  }
+});
